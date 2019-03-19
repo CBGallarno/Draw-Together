ef01276cabaf26bdb82a7ed8bebc1fd94fbd6b6c
Chris Gallarno <cgallarno200@gmail.com>
2019-03-19 09:27:49 -0400

    Reformat and added Firebase dependency

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from "./components/home/Home";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navPages: {
+        'Home': Home,
+      }
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
